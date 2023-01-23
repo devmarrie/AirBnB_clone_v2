a225d72d528812d80a453e22a17da6a51a49800e
devmarrie <annemarrieakeyo42@gmail.com>
2023-01-23 18:07:41 +0300

    [feat]:more routes

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+"""Starts a Flask web app
+Listens on 0.0.0.0, port 5000
+Routes:
+
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+app.route('/', strict_slashes=False)
+def home():
+    """greets"""
+    return "Hello HBNB!"
+
+
+app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """just hbnb"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
+
+
+
