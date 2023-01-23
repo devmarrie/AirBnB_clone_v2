167ca4683a3e904549d0b9e9d654cf8b47d48213
devmarrie <annemarrieakeyo42@gmail.com>
2023-01-23 19:08:02 +0300

    [feat]:adding python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Starts Flask web app
+Routes:
+    / - display "Hello HBNB!"
+    /hbnb - display "HBNB"
+    /c/<text> - display "C <text>"
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb_route():
+    """prints Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """prints HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    """prints C followed by <text> content"""
+    txt = text.replace("_", " ")
+    return "C %s" % txt
+
+
+@app.route('/python/<string:text>', strict_slashes=False)
+def p_text(text):
+    text = text.replace("_", " ")
+    return "%s is cool" %text
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
