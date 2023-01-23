a3bd472ee6fb838201cf19a1578c74af47b5d12b
devmarrie <annemarrieakeyo42@gmail.com>
2023-01-23 19:07:46 +0300

    [fix]:changes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def greetings():
     """ Greets Hello HBNB!"""
     return 'Hello HBNB!'
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,13 +11,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def home():
     """greets"""
     return "Hello HBNB!"
 
 
-app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """just HBNB"""
     return "HBNB"
