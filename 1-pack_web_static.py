a975a36c89425e8b4e3f62bf57bdbbaa0015f9b3
devmarrie <annemarrieakeyo42@gmail.com>
2023-03-15 18:12:48 +0300

    Revert "new"
    This reverts commit 4dc6bc804c8c60127f9f1327fe9fc8f8bf5d6019.

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,19 @@
+#!/usr/bin/python3
 """Fabric script that generates a .tgz archive from
 the contents of the web_static folder"""
-from fabric.api import local
 from datetime import datetime
+from fabric.api import local
+import os
 
 
 def do_pack():
-     """generating a .tgz archive from the contents of the web_static"""
-     local('mkdir -p versions')
-     time = datetime.now().strftime("%Y%m%d%H%M%S")
-     name = "web_static_{}.tgz".format(time)
-     file_name = "versions/{}".format(name)
-     location = local("tar -cvzf {} web_static/".format(file_name))
-     if location:
-         return (location)
-     else:
-         return None
+    try:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        t = datetime.now()
+        f = "%Y%m%d%H%M%S"
+        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        return archive_path
+    except:
+        return None
