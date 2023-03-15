777e8e879578e82327a376956eb8d10b8c292ee5
devmarrie <annemarrieakeyo42@gmail.com>
2023-03-15 18:37:06 +0300

    yei

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Fabric script that generates a .tgz archive from
 the contents of the web_static folder"""
+
 from datetime import datetime
 from fabric.api import local
 import os
